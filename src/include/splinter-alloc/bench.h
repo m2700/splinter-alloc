22aaebddbe46fa6b88630f6c738688e136546416
Matthias Kübrich <matthias.kuebrich@t-online.de>
2023-03-28 19:18:01 +0200

    scaling alloc benchmark

--- src/include/splinter-alloc/bench.h
+++ src/include/splinter-alloc/bench.h
@@ -6,3 +6,4 @@
 void spla_alloc_free_fifo(splinter_alloc *spla_alloc, size_t num_allocs, size_t block_size);
 void spla_alloc_free_lifo(splinter_alloc *spla_alloc, size_t num_allocs, size_t block_size);
 void spla_alloc_free_random(splinter_alloc *spla_alloc, size_t num_allocs, size_t block_size);
+void spla_alloc_scale(splinter_alloc *spla_alloc, size_t num_allocs, size_t block_size);
