8bb47acec85667f3fb2b2dd16cdfac76594576cb
Matthias Kübrich <matthias.kuebrich@t-online.de>
2023-04-08 10:54:34 +0200

    align down

--- src/align.h
+++ src/align.h
@@ -3,11 +3,15 @@
 #include "bitidx.h"
 
 #define SHIFT_TO_REST_MASK(shift) (((size_t)1 << (shift)) - 1)
-#define SHIFT_TO_MASK(shift) ~SHIFT_TO_REST_MASK(shift)
+#define SHIFT_TO_MASK(shift) (~SHIFT_TO_REST_MASK(shift))
+
 #define ALIGN_UP(x, shift)                                                                         \
     (typeof(x))(((size_t)(x) + SHIFT_TO_REST_MASK(shift)) & SHIFT_TO_MASK(shift))
+#define ALIGN_DOWN(x, shift) ((typeof(x))((size_t)(x)&SHIFT_TO_MASK(shift)))
+
 #define ALIGN_UP_SIZE(x, align_size)                                                               \
     (typeof(x))(((size_t)(x) + ((align_size)-1)) & ~((align_size)-1))
+
 #define DIV_SHIFT_CEIL(x, shift) ((((x) + SHIFT_TO_REST_MASK(shift)) >> (shift)))
 
 /// x must not be 0
